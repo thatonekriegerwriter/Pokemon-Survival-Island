35b4b60053f223bbad25c13fb0f95a22873f6a35
thatonekriegerwriter <60249166+thatonekriegerwriter@users.noreply.github.com>
2021-10-30 21:13:03 -0600

    jnnnj

--- Plugins/Handlers/handler.rb
+++ Plugins/Handlers/handler.rb
@@ -73,3 +73,97 @@ end
 
 }
 
+def GrassEvolutionStone
+  case $game_variables[3]
+     when :GLOOM
+	     $game_variables[4] = 1
+     when :WEEPINGBELL
+	     $game_variables[4] = 2
+     when :EXEGGCUTE 
+	     $game_variables[4] = 3
+     when :EEVEE
+	     $game_variables[4] = 4
+     when :NUZLEAF
+	     $game_variables[4] = 5
+     when :PANSAGE
+	     $game_variables[4] = 6
+     when :CHERUBI
+	     $game_variables[4] = 7
+	 else
+	     $game_variables[4] = 0
+  end
+if $game_variables[4]== 0
+    Kernel.pbMessage(_INTL("That does not seem to be able to evolve with this stone."))
+end
+if $game_variables[4]== 1
+    Kernel.pbMessage(_INTL("Gloom evolves into Vileplume."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:VILEPLUME)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 2
+    Kernel.pbMessage(_INTL("Weepingbell evolves into Victorybell."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:VECTREEBEL)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 3
+    Kernel.pbMessage(_INTL("Exeggcute evolves into Exeggcutor."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:EXEGGCUTOR)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 4
+    Kernel.pbMessage(_INTL("Eevee evolves into Leafeon."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:LEAFEON)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 5
+    Kernel.pbMessage(_INTL("Nuzleaf evolves into Shiftry."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:SHIFTRY)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 6
+    Kernel.pbMessage(_INTL("Pansage evolves into Semisage."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:SEMISAGE)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+end
+if $game_variables[4]== 7
+    Kernel.pbMessage(_INTL("Cherubi normally evolves into Cherrim."))
+	pkmn=$trainer.party[$game_variables[1]]
+	pbFadeOutInWithMusic {
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pbGetPokemon(1),:STEENEE)
+    evo.pbEvolution(false)
+    evo.pbEndScreen
+}
+    Kernel.pbMessage(_INTL("OH! How abnormal!"))
+end
+end
